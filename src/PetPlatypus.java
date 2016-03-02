ed7383aa2ed20481f364f013606aecfc3517568f
No One <No.Ones.Email@gmail.com>
2016-03-01 17:26:09 -0800

    pet platypus failed cuz of something

---
+++ src/PetPlatypus.java
@@ -0,0 +1,19 @@
+public class PetPlatypus {
+	private String name;
+	
+	PetPlatypus(String name){
+		name = this.name;
+	}
+	
+	void sayHi(){
+		System.out.println("The platypus " + name + " is smarter than your average platypus.");
+	}
+	
+	public static void main(String[] args) {
+		//1. Make  an instance of your new pet platypus
+		 PetPlatypus a = new PetPlatypus("Perry");
+		//2. Call the sayHi method
+		a.sayHi();
+		//3. Create a constructor in the platypus class so that you can give your platypus a name.
+	}
+}
