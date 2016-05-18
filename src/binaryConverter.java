b1a0c57bc5ad34c6331cd019581de320b3092b07
LEAUGE COMPUTER <DO NOT CHANGE THIS/>
2016-05-17 18:34:51 -0700

    yo betsy the besty il see ya next week yo but today we did da cool stuff called popcorn and a worksheet and binary :( ps see ya gtg :)))))))))))))

--- src/binaryConverter.java
+++ src/binaryConverter.java
@@ -1,3 +1,4 @@
+import java.awt.Button;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.regex.Pattern;
@@ -18,39 +19,29 @@ public class binaryConverter implements ActionListener {
 
 	public static void main(String[] args) {
 		new binaryConverter().go();
+
 	}
 
 	private void go() {
 		frame.setVisible(true);
 		frame.setSize(500, 500);
 		frame.add(panel);
-		panel.add(answer);
 		panel.add(start);
+		panel.add(answer);
 		panel.add(button);
+		button.setText("convert from binary!");
 		button.addActionListener(this);
+		answer.setEditable(false);
 	}
 
 	public void actionPerformed(ActionEvent e) {
-		int bl = convert(start.getText());
+		String bl = convert(start.getText());
 		JOptionPane.showMessageDialog(null, bl);
 	}
 
-	int convert(String binary) {
-		if (binary.length() != 8) {
-			JOptionPane.showMessageDialog(null, "Enter 8 bits, silly!!!");
-			return 0;
-		}
-		if (!Pattern.matches("\2", binary)) {
-			JOptionPane.showMessageDialog(null, "Binary can only contain 1s or 0s, silly!!!");
-			return 0;
-		}
-		try {
-			int asciiValue = Integer.parseInt(binary, 2);
-			return asciiValue;
-		} catch (Exception e) {
-			JOptionPane.showMessageDialog(null, "Enter a binary, silly!!!");
-			return 0;
-		}
+	String convert(String binary) {
+		int asciiValue = Integer.parseInt(binary, 2);
+		char theLetter = (char) asciiValue;
+		return "" + theLetter;
 	}
-
 }
