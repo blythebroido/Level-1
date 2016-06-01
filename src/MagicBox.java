73639718b2b6fdc30ff50e3373495bafbeaad5e0
INeedAUniqueUsername <alexiscomical445@gmail.com>
2016-05-31 17:26:22 -0700

    firstSync

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -1,16 +1,26 @@
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 import javax.imageio.ImageIO;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
-public class MagicBox extends JPanel implements Runnable {
-
+public class MagicBox extends JPanel implements Runnable, MouseListener {
+	int mouseX;
+	int mouseY;
+	int correctColor;
+	int guessColor;
+	boolean mouseHasBeenPressed = false;
 	/*
 	 * Work together as a TEAM of 2 or 3 to make this project. We are going to hide secrets within the magic box. 
 	 * When the user clicks on a secret place, stuff will happen.
@@ -26,7 +36,7 @@ public class MagicBox extends JPanel implements Runnable {
 
 	public static void main(String[] args) throws Exception {
 		SwingUtilities.invokeLater(new MagicBox());
-		if(__.equals(_)){
+		if(1==1){
 			
 		}
 		else{
@@ -71,5 +81,42 @@ public class MagicBox extends JPanel implements Runnable {
 		Icon icon = new ImageIcon(imageURL);
 		return new JLabel(icon);
 	}
+
+
+
+@Override
+public void mouseClicked(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
 }
+
+@Override
+public void mousePressed(MouseEvent e) {
+	// TODO Auto-generated method stub
+	mouseX = e.getX();
+	mouseY = e.getY();
+	guessColor = backgroundImage.getRGB(mouseX,mouseY);
+	mouseHasBeenPressed = true;
+	System.out.println(guessColor);
+}
+
+@Override
+public void mouseReleased(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void mouseEntered(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void mouseExited(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+}
+
 
